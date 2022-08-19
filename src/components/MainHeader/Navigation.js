879fdc859f7721f429c3e0de003c86a00c976370
ng-webdev <110598855+ng-webdev@users.noreply.github.com>
2022-08-19 10:36:35 +0200

    first commit

---
+++ src/components/MainHeader/Navigation.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+import classes from "./Navigation.module.css";
+
+const Navigation = (props) => {
+  return (
+    <nav className={classes.nav}>
+      <ul>
+        {props.isLoggedIn && (
+          <>
+            <li>
+              <a href="/">Users</a>
+            </li>
+            <li>
+              <a href="/">Admin</a>
+            </li>
+          <li>
+            <button onClick={props.onLogout}>Logout</button>
+          </li>
+          </>
+        )}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navigation;
